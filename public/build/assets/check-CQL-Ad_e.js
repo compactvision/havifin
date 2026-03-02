298510fcf5cf9e1798161fa28ed79cb51f364221
compactvision <heymytechcorporation@gmail.com>
2026-03-02 13:57:25 +0100

    Fix cors

---
+++ public/build/assets/check-CQL-Ad_e.js
@@ -0,0 +1,1 @@
+import{c}from"./createLucideIcon-Bw83V9sH.js";const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("Check",e);export{t as C};
---
+++ public/build/assets/circle-check-BBltzwXN.js
@@ -0,0 +1,1 @@
+import{c}from"./createLucideIcon-Bw83V9sH.js";const e=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]],r=c("CircleCheck",e);export{r as C};
